24abbe9143f0fe33368f52bd327d3212b3013b86
Gabriel Bezerra <gabrielbezerra@hotmail.com>
2023-08-23 17:47:07 -0300

    Primeiro componente quase pronto

--- app/src/main/java/com/example/app30praum/MainActivity.kt
+++ app/src/main/java/com/example/app30praum/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,7 +15,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -24,14 +27,25 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.sp
 import com.example.app30praum.ui.theme._30praumTheme
+import com.example.app30praum.ui.theme.primaryGray
+import kotlinx.coroutines.coroutineScope
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +53,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             _30praumTheme {
                 Surface {
-
+                    ProductItem()
                 }
             }
         }
@@ -49,20 +63,48 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun ProductItem() {
-    val primaryBlue = Color(0xFFa4a3a3)
     Box(
         modifier = Modifier
-            .clip(RoundedCornerShape(12.dp))
-            .background(color = primaryBlue)
-            .padding(7.dp)
+            .padding(50.dp)
+            .height(265.dp)
+            .width(200.dp)
+            .clip(RoundedCornerShape(18.dp))
+            .background(color = primaryGray)
     ) {
-        Column {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+        ) {
             Image(
                 painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = null
+                contentDescription = null, modifier = Modifier
+                    .size(180.dp)
+                    .padding(2.dp)
+                    .align(CenterHorizontally)
             )
-            Text(text = "Texto 1", color = Color.White)
-            Text(text = "Texto 2", color = Color.White)
+            Column(modifier = Modifier.padding(12.dp, 0.dp).fillMaxWidth()) {
+                Text(
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .align(CenterHorizontally),
+                    text = LoremIpsum(8).values.first(),
+                    color = Color.White,
+                    fontWeight = FontWeight(700),
+                    fontSize = 16.sp,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$000,00",
+                    color = Color.White,
+                    fontWeight = FontWeight(400),
+                    fontSize = 14.sp,
+                    modifier = Modifier.padding(2.dp)
+                )
+            }
         }
+
     }
 }
