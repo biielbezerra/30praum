091102e9ad713642177c9ab030c21ee5b88f17c9
biielbezerra <gabrielbezerra38@hotmail.com>
2023-08-22 22:31:12 -0300

    praticando com layouts

--- app/src/main/java/com/example/app30praum/MainActivity.kt
+++ app/src/main/java/com/example/app30praum/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.app30praum
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,7 +21,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             _30praumTheme {
-                Surface{
+                Surface {
                     MyFirstComposable()
                 }
             }
@@ -25,14 +29,55 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun MyFirstComposable(){
+fun ColumnPreview() {
+    Column {
+        Text(text = "Text 1")
+        Text(text = "Text 2")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RowPreview() {
+    Row {
+        Text(text = "Text 3")
+        Text(text = "Text 4")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomLayoutPreview() {
+    Column {
+        Text(text = "Text 1")
+        Text(text = "Text 2")
+        Row {
+            Text(text = "Text 3")
+            Text(text = "Text 4")
+        }
+        Box {
+            Row {
+                Text(text = "Text 5")
+                Text(text = "Text 6")
+            }
+            Column {
+                Text(text = "Text 7")
+                Text(text = "Text 8")
+            }
+        }
+    }
+}
+
+@Composable
+fun MyFirstComposable() {
     Text(text = "My first composable")
 }
 
-@Preview()
+@Preview
 @Composable
-fun MyFirstComposablePreview(){
+fun MyFirstComposablePreview() {
     _30praumTheme {
         Surface {
             MyFirstComposable()
