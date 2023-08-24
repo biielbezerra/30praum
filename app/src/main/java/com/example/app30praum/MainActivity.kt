352fd81f601971937f79dc36530aa283eb89ee7f
Gabriel Bezerra <gabrielbezerra@hotmail.com>
2023-08-24 17:37:55 -0300

    Product section pronta, começando a criar classe de produtos

--- app/src/main/java/com/example/app30praum/MainActivity.kt
+++ app/src/main/java/com/example/app30praum/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,6 +26,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Checkbox
@@ -30,8 +36,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.painterResource
@@ -45,9 +53,13 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.sp
+import com.example.app30praum.model.Product
+import com.example.app30praum.ui.theme.Purple40
+import com.example.app30praum.ui.theme.Purple80
 import com.example.app30praum.ui.theme._30praumTheme
 import com.example.app30praum.ui.theme.primaryGray
 import kotlinx.coroutines.coroutineScope
+import java.math.BigDecimal
 import kotlin.math.max
 
 class MainActivity : ComponentActivity() {
@@ -56,17 +68,60 @@ class MainActivity : ComponentActivity() {
         setContent {
             _30praumTheme {
                 Surface {
-                    ProductItem()
+                    ProductSection()
                 }
             }
         }
     }
 }
 
-@Preview
 @Composable
-fun ProductItem() {
-    Surface(Modifier.padding(20.dp),shape = RoundedCornerShape(30.dp), shadowElevation = 6.dp) {
+fun ProductSection() {
+    Column {
+        Text(
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    top = 16.dp,
+                    end = 16.dp
+                ),
+            text = "Camisetas",
+            fontSize = 20.sp,
+            fontWeight = FontWeight(400)
+        )
+        Row(
+            modifier = Modifier
+                .padding(
+                    top = 16.dp,
+                    bottom = 16.dp
+                )
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState())
+                .padding(horizontal = 16.dp)
+            , horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            ProductItem(Product(
+                name = "Camiseta",
+                price = BigDecimal("129.90"),
+                image = R.drawable.ieu_2580_1536x2048_png
+            ))
+            ProductItem(Product(
+                name = "Camiseta",
+                price = BigDecimal("129.90"),
+                image = R.drawable.metralhadora_preta_frente_1000x1000_png
+            ))
+            ProductItem(Product(
+                name = "Bermuda",
+                price = BigDecimal("229.90"),
+                image = R.drawable.bermuda_verde_frente_1000x1000_png
+            ))
+        }
+    }
+}
+
+@Composable
+fun ProductItem(product: Product) {
+    Surface(shape = RoundedCornerShape(30.dp), shadowElevation = 6.dp) {
         Box(
             modifier = Modifier
                 .heightIn(min = 250.dp, max = 260.dp)
@@ -80,20 +135,22 @@ fun ProductItem() {
                     .fillMaxWidth()
             ) {
                 Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    painter = painterResource(id = product.image),
                     contentDescription = null, modifier = Modifier
                         .size(180.dp)
                         .padding(2.dp)
                         .align(CenterHorizontally)
                 )
-                Column(modifier = Modifier
-                    .padding(12.dp, 0.dp)
-                    .fillMaxWidth()) {
+                Column(
+                    modifier = Modifier
+                        .padding(12.dp, 0.dp)
+                        .fillMaxWidth()
+                ) {
                     Text(
                         textAlign = TextAlign.Center,
                         modifier = Modifier
                             .align(CenterHorizontally),
-                        text = LoremIpsum(8).values.first(),
+                        text = product.name,
                         color = Color.White,
                         fontWeight = FontWeight(700),
                         fontSize = 18.sp,
@@ -101,7 +158,7 @@ fun ProductItem() {
                         overflow = TextOverflow.Ellipsis
                     )
                     Text(
-                        text = "R$000,00",
+                        text = product.price.toPlainString(),
                         color = Color.White,
                         fontWeight = FontWeight(400),
                         fontSize = 14.sp,
@@ -111,4 +168,82 @@ fun ProductItem() {
             }
         }
     }
+}
+
+@Composable
+fun ArtistCard() {
+    Surface(Modifier.padding(20.dp), shape = RoundedCornerShape(30.dp), shadowElevation = 6.dp) {
+        Row(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Purple80, Purple40
+                            )
+                        )
+                    )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null, modifier = Modifier
+                        .size(imageSize)
+                        .align(Alignment.Center)
+                        .offset(x = imageSize / 2)
+                        .clip(shape = CircleShape)
+                        .border(
+                            BorderStroke(
+                                2.dp,
+                                brush = Brush.verticalGradient(
+                                    listOf(
+                                        Purple80,
+                                        Purple40
+                                    )
+                                )
+                            ), CircleShape
+                        )
+                )
+            }
+            Spacer(modifier = Modifier.width(imageSize / 2))
+            Box(
+                Modifier
+                    .padding(16.dp)
+                    .fillMaxHeight()
+                    .padding(16.dp)
+                    .align(CenterVertically),
+            ) {
+                Column {
+                    Text(
+                        text = LoremIpsum(50).values.first(),
+                        overflow = TextOverflow.Ellipsis,
+                        lineHeight = 20.sp
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductSectionPreview() {
+    ProductSection()
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductItemPreview() {
+    ProductItem(
+        Product(
+        name = "Camiseta",
+        price = BigDecimal("129.90"),
+        R.drawable.ic_launcher_background
+    ))
 }
