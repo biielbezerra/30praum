43f318379992a89bf6316c5a9dbb858ea583b2f0
biielbezerra <gabrielbezerra38@hotmail.com>
2023-08-29 10:16:48 -0300

    update project

--- app/src/main/java/com/example/app30praum/MainActivity.kt
+++ app/src/main/java/com/example/app30praum/MainActivity.kt
@@ -3,19 +3,8 @@ package com.example.app30praum
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.app30praum.ui.components.ArtistSection
-import com.example.app30praum.ui.components.ProductSection
 import com.example.app30praum.ui.screens.HomeScreen
 import com.example.app30praum.ui.theme._30praumTheme
 
